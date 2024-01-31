bc10c057c3a17ff7a4aa54f44c86534ea1630bba
Artur Komaristych <1359645+S0PEX@users.noreply.github.com>
2024-01-31 23:54:50 +0100

    Push latest fixes

--- files/code/complexRegex.java
+++ files/code/complexRegex.java
@@ -1,6 +1,6 @@
 @Given( "^(?:the )?(same )?(running )?instance[s]? \"([^\"]*)\"" +
-        "using (?:the (default|legacy|base) build|build \"([^\"]*)\")" +
-        "(?: with im master role \"([^\"]*)\")?$")
+        "using (?:the (default|legacy|base) build|build " +
+        "\"([^\"]*)\")(?: with im master role \"([^\"]*)\")?$")
 public void givenInstancesUsingBuild(String keepProfile, ...)
 throws Exception{
     ...
